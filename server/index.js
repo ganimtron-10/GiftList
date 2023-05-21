21eb04adec1642e7d78b2d72bf80f7b4c6d2cbd3
Praneeth Shetty <praneethshetty10@gmail.com>
2023-05-21 10:43:03 +0530

    adding server side proof verification

--- server/index.js
+++ server/index.js
@@ -8,15 +8,17 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
+  let clientName = body.name;
+  let clientProof = body.proof;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  const isInTheList = verifyProof(clientProof, clientName, MERKLE_ROOT);
+  if (isInTheList) {
     res.send("You got a toy robot!");
   }
   else {
